3e80ec322cf9e4e918498116f0b127a02a45e088
Aneesh AV <aneesh@amcde.com>
2021-06-28 11:04:40 +0300

    [ADD] Direct Transfer

---
+++ direct_material_transfer/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Direct Material Transfer',
+    'category': 'Inventory',
+    'sequence': 1,
+    'version': '14.0.1.0.0',
+    'license': 'LGPL-3',
+    'summary': """Direct Material Transfer""",
+    'description': """Direct Material Transfer""",
+    'author': 'Aneesh.AV',
+    'depends': ['stock','account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/ir_sequence.xml',
+        'views/direct_material_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
