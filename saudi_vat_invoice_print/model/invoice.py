f2b1acf362344da36ae51010f8b25924ce408c5d
Aneesh AV <aneesh@amcde.com>
2022-01-04 20:03:31 +0300

    [add] Invoice template updates

--- saudi_vat_invoice_print/model/invoice.py
+++ saudi_vat_invoice_print/model/invoice.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, models, fields, _
+import binascii
+
 from num2words import num2words
-from odoo.http import request
+from odoo import api, models, fields
+from datetime import datetime
 from . import qr_code_base
+import pytz
+import base64
 
 
 class AccountMoveLine(models.Model):
@@ -37,6 +41,12 @@ class AccountMoveLine(models.Model):
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
+    TAG_SELLER = 1
+    TAG_VAT_NO = 2
+    TAG_TIME_STAMP = 3
+    TAG_TOTAL = 4
+    TAG_VAT_TOTAL = 5
+
     amount_text = fields.Char(string='Amount In Words', compute='amount_to_words')
     amount_in_ar = fields.Char(string='Amount In Words', compute='amount_to_words')
     attention = fields.Many2one('res.partner', 'Attention')
@@ -45,11 +55,83 @@ class AccountMove(models.Model):
     vat_arabic_text = fields.Char('Vat Text(Arabic)',compute='_get_vat_text')
     discount = fields.Float('Discount',compute='_compute_all_price')
     price_before_discount = fields.Monetary('Total ( Excluded VAT)',compute='_compute_all_price')
-    qr_image = fields.Binary("QR Code", compute='_generate_qr_code')
+    # qr_image = fields.Binary("QR Code", compute='_generate_qr_code')
     delivery_date = fields.Date('Delivery Date')
 
-    def _generate_qr_code(self):
-        self.qr_image = qr_code_base.generate_qr_code(self.partner_id.name)
+
+    def _data_hex(self, value, tag):
+
+        hex_tag = self._convert_int_to_hex(tag)
+        hex_len = self._convert_int_to_hex(len(value.encode("UTF-8")))
+        hex_val = self._convert_str_to_hex(value)
+        return "%s%s%s" % (hex_tag, hex_len, hex_val)
+
+    def _seller_hex(self, value):
+        return self._data_hex(value, self.TAG_SELLER)
+
+    def _vat_no_hex(self, value):
+        return self._data_hex(value, self.TAG_VAT_NO)
+
+    def _time_stamp_hex(self, value):
+        return self._data_hex(value, self.TAG_TIME_STAMP)
+
+    def _total_hex(self, value):
+        return self._data_hex(value, self.TAG_TOTAL)
+
+    def _vat_total_hex(self, value):
+        return self._data_hex(value, self.TAG_VAT_TOTAL)
+
+    def _convert_int_to_hex(self, value):
+        return "%0.2x" % value
+
+    def _convert_str_to_hex(self, value):
+        hex_val = ""
+        if value:
+            str_bytes = value.encode("UTF-8")
+            encoded_hex = binascii.hexlify(str_bytes)
+            hex_val = encoded_hex.decode("UTF-8")
+
+        return hex_val
+
+    def _convert_text_to_base64(self, value):
+        # value_bytes = value.decode('utf-8')
+        base64_bytes = base64.b64encode(value)
+        return base64_bytes.decode("utf-8")
+
+    def generate_tlv_code(self):
+        self.invalidate_cache()
+        DEFAULTE_TZ = 'Asia/Riyadh'
+        hex_seller = False
+        hex_vat_no = False
+        hex_time_stamp = False
+        total = False
+        vat_total = False
+        # time_stamp = datetime.now(
+        #     pytz.timezone(self.env.user.tz or self._context.get('tz', DEFAULTE_TZ))).strftime('%Y-%m-%dT%H:%M:%SZ')
+
+        localFormat = "%Y-%m-%d %H:%M:%S"
+
+        # Convert date to current user timezone or Saudi Arabia in default case.
+        utcmoment_naive = datetime.strptime(self.create_date, localFormat)
+        utcmoment = utcmoment_naive.replace(tzinfo=pytz.utc)
+
+        tz_ = self.env.user.tz or self._context.get('tz', DEFAULTE_TZ)
+        localDatetime = utcmoment.astimezone(pytz.timezone(tz_))
+
+        time_stamp = localDatetime.strftime('%Y-%m-%dT%H:%M:%SZ')
+        hex_seller = self._seller_hex(self.company_id.name or "")
+        hex_vat_no = self._vat_no_hex(self.company_id.vat or "")
+        hex_time_stamp = self._time_stamp_hex(time_stamp)
+        hex_total = self._total_hex(str(self.amount_total))
+        hex_vat_total = self._vat_total_hex(str(self.amount_tax))
+
+        hex_text = "%s%s%s%s%s" % (hex_seller, hex_vat_no, hex_time_stamp, hex_total, hex_vat_total)
+        code_qr = self._convert_text_to_base64(bytearray.fromhex(hex_text))
+
+        return code_qr
+
+    # def _generate_qr_code(self):
+    #     self.qr_image = qr_code_base.generate_qr_code(self.partner_id.name)
 
     # def sticker_barcode_generator(self):
     #     # patient = self.env['lab.patient'].search([('patient', '=', self.partner_id.id)])
@@ -107,6 +189,12 @@ class AccountMove(models.Model):
                 if line.account_id.id == self.account_id.id:
                     line.write({'analytic_account_id': analytic.id, 'partner_id': self.partner_id.id})
         return super(AccountMove, self).invoice_validate()
+
+    @api.onchange('partner_id')
+    def _onchange_partner_id_other(self):
+        if self.partner_id:
+            self.attention = self.partner_id.id
+
 
     def invoice_print(self):
         self.ensure_one()
