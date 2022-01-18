2a41ccaea30e5c4106e7a6315607e61fbd1c1cc3
Aneesh AV <aneesh@amcde.com>
2022-01-18 09:53:03 +0300

    [ADD] Branch module update

--- aspl_company_branch_ee/models/res_partner.py
+++ aspl_company_branch_ee/models/res_partner.py
@@ -15,23 +15,28 @@ from odoo import models, fields, api
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    @api.model
-    def search(self, args, offset=0, limit=None, order=None, count=False):
-        user_rec = self.env['res.users'].sudo().browse(self._uid).branch_ids.ids
-        new_args = []
-        if user_rec:
-            sql = """SELECT id FROM res_partner
-                    WHERE branch_id in %s""" % (" (%s) " % ','.join(map(str, user_rec)))
-            self._cr.execute(sql)
-            result = self._cr.fetchall()
-            rec = [each[0] for each in result]
-            args += ['|', ('id', 'in', rec), ('branch_id', '=', False)]
-            return super(ResPartner, self).search(args=args, offset=offset, limit=limit,
-                                                  order=order, count=count)
-        return super(ResPartner, self).search(args=args, offset=offset, limit=limit,
-                                              order=order, count=count)
+    # @api.model
+    # def search(self, args, offset=0, limit=None, order=None, count=False):
+    #     user_rec = self.env['res.users'].sudo().browse(self._uid).branch_ids.ids
+    #     new_args = []
+    #     if user_rec:
+    #         sql = """SELECT id FROM res_partner
+    #                 WHERE branch_id in %s""" % (" (%s) " % ','.join(map(str, user_rec)))
+    #         self._cr.execute(sql)
+    #         result = self._cr.fetchall()
+    #         rec = [each[0] for each in result]
+    #         args += ['|', ('id', 'in', rec), ('branch_id', '=', False)]
+    #         return super(ResPartner, self).search(args=args, offset=offset, limit=limit,
+    #                                               order=order, count=count)
+    #     return super(ResPartner, self).search(args=args, offset=offset, limit=limit,
+    #                                           order=order, count=count)
 
     # branch_id = fields.Many2one('company.branch', string="Branch",
     #                             default=lambda self: self.env.user.branch_id)
 
     branch_id = fields.Many2many('company.branch', string="Branch", required=True)
+
+    @api.onchange('branch_id')
+    def onchange_branch_ids(self):
+        return {'domain': {
+            'branch_id': [('id', 'in', self.env.user.branch_ids.ids)]}}
