7cc4a62909d5d11efba1d04b92496a50bdb4ddf5
Hesham Abou Khair <hesham.a.kh@gmail.com>
2022-11-09 14:40:35 +0200

    [ADD] BAKH-4 add partner ledger customization

---
+++ azk_partner_ledger_customization/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Partner Ledger Customization",
+    "version": "14.0.0.0.0",
+    "category": "",
+    "author": "Azkatech",
+    "website": "https://azka.tech",
+    "license": "AGPL-3",
+    "depends": ['base','account','account_reports'],
+    "data": [
+        "views/report_views.xml" 
+    ],
+}
---
+++ azk_partner_ledger_customization/models/account_partner_ledger.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, api, fields, _
+
+
+
+class ReportAccountPartnerLedger(models.AbstractModel):
+    _inherit = "account.partner.ledger"
+    
+    
+    def get_pdf(self, options, minimal_layout=True):
+        options['az_print_mode'] = True
+        return super().get_pdf(options, False)
+    
+    def get_html(self, options, line_id=None, additional_context=None):
+        if self.env.context.get('print_mode'):
+            if additional_context:
+                additional_context.update({'az_print_mode': True})
+            else:
+                additional_context = {'az_print_mode': True}
+
+        return super().get_html(options, line_id, additional_context)
