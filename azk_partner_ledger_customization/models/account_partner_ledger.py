420743d26a898e227a394c8845e0fd9c0df9553a
Hesham Abou Khair <hesham.a.kh@gmail.com>
2022-12-07 17:20:13 +0400

    [FIX] BAKH-4 W1 update partner ledger & concret delivery note

--- azk_partner_ledger_customization/models/account_partner_ledger.py
+++ azk_partner_ledger_customization/models/account_partner_ledger.py
@@ -10,7 +10,7 @@ class ReportAccountPartnerLedger(models.AbstractModel):
     
     def get_pdf(self, options, minimal_layout=True):
         options['az_print_mode'] = True
-        return super().get_pdf(options, False)
+        return super().get_pdf(options, True)
     
     def get_html(self, options, line_id=None, additional_context=None):
         if self.env.context.get('print_mode'):
