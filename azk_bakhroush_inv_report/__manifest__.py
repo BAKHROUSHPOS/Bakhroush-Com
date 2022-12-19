f488a998b4cc4ad63ac5f4145de1e8b0a3039488
Hesham Abou Khair <hesham.a.kh@gmail.com>
2022-12-19 12:32:07 +0200

    [ADD] BAKH-4 W1 add new vat customization

---
+++ azk_bakhroush_inv_report/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Bakhroush Invoice Report",
+    "version": "14.0.0",
+    "depends": [
+        'base', 'web', 'account', 'einv_sa', 'saudi_vat_invoice_print'
+    ],
+    "author": "Azkatech SAL",
+    "website": "http://www.azka.tech",
+    "support": "info@azka.tech",
+    "category": "Accounting",
+   
+    "images": [],
+    "license": "AGPL-3",
+
+    "summary": "",
+    "description": """ """
+  
+   
+    ,
+    "data": [
+        "view/invoice_report.xml",
+       
+    ],
+    "installable": True,
+    "auto_install": True,
+    "application": False,
+}
